7215cadf2b9df9813a5682190d34e118b8781030
649498722@qq.com <649498722@qq.com>
2025-06-20 08:03:39 -0700

    Substitute the python implementation with C.

--- dilithium-py-main/src/dilithium_py/dilithium/threshold/threshold_dilithium/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dilithium import Dilithium2, Dilithium3, Dilithium5
-
-__all__ = ["Dilithium2", "Dilithium3", "Dilithium5"]
--- dilithium-py-main/src/dilithium_py/dilithium/threshold/threshold_dilithium/dilithium.py
+++
@@ -1,6 +0,0 @@
-from .core import Dilithium
-from .params import DEFAULT_PARAMETERS
-
-Dilithium2 = Dilithium(DEFAULT_PARAMETERS["dilithium2"])
-Dilithium3 = Dilithium(DEFAULT_PARAMETERS["dilithium3"])
-Dilithium5 = Dilithium(DEFAULT_PARAMETERS["dilithium5"])
