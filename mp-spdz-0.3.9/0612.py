638614982a678fad2393ff1e13dfd1bab90f321f
649498722@qq.com <649498722@qq.com>
2025-06-24 07:18:28 -0700

    Update, to be tested on the server.

--- mp-spdz-0.3.9/0612.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-subprocess.run(["Scripts/compile-run.py", "mascot", "test"])
---
+++ mp-spdz-0.3.9/testrun.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+
+int main() {
+    system("Scripts/compile-run.py -E mascot tutorial");
+    return 0;
+}
